88261467cca2848f30b77c3ed3fd1e2812a7c927
FranklinLiao <1120205104@qq.com>
2015-05-06 16:39:26 +0800

    LeetCode1_TwoSum.java import java.util.Hashtable Hashtable<Integer,Integer> hashtable = new Hashtable<Integer,Integer>();
    Signed-off-by: FranklinLiao <1120205104@qq.com>

---
+++ LeetCode1_TwoSum.java
@@ -0,0 +1,19 @@
+﻿import java.util.Hashtable;
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int result[] = {0,0};
+        Hashtable<Integer,Integer> hashTable = new Hashtable<Integer,Integer>(); //定义存储数据的hashtable
+        for(int index=0;index<nums.length;index++) { //便利数组，将数据存入hashtable
+            hashTable.put(nums[index],index); //key为数组中元素的值，value为该元素在数组中的位置
+        }
+        for(int index=0;index<nums.length;index++) { //便利查找元素
+            int pairValue = target - nums[index];
+            if((hashTable.get(pairValue)!=null)&&(hashTable.get(pairValue)>index)) { //找到了另一个元素 并且找到的不能是自己
+                result[0] = index+1;
+                result[1] = hashTable.get(pairValue)+1;
+                break;//找到了就退出
+            }
+        }
+        return result;
+    }
+}
